f8832f2ce84f5e0d9434f32b7f654056bd2b7a0d
joaquinCarbonell <joachim.carbonell@googlemail.com>
2020-05-02 17:56:13 +0200

    featured rooms

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/rooms" component={Rooms}/>
-        <Route exact path="/rooms/:type" component={Room}/>
+        <Route exact path="/rooms/:slug" component={Room}/>
         <Route  component={Error}/>
       </Switch>
     </div>
