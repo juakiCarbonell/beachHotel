69078142454326d23b328cff4a181acfb21fb551
joaquinCarbonell <joachim.carbonell@googlemail.com>
2020-05-03 17:43:46 +0200

    hero with styled compoents

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 
 import { RoomContext } from '../context';
 
+import StyledHero from '../components/StyledHero';
 
 class SingleRoom extends Component {
 
@@ -36,13 +37,13 @@ class SingleRoom extends Component {
     }
     const {name, description, price, extras, breakfast, pets, images} = room;
     return (
-      <Hero hero='roomsHero'>
+      <StyledHero img={images[0] || this.state.defaultBcg}>
         <Banner title={`${name} room`}>
           <Link to="/rooms" className="btn-primary">
             Back to rooms
           </Link>
         </Banner>
-      </Hero>
+      </StyledHero>
     );
   }
 }
